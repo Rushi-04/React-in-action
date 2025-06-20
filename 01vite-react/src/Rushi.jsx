3d74337efaf3bfa92244aad04b73bacda6a3cd08
Rushi-04 <borkarananta028@gmail.com>
2025-06-20 18:33:34 +0530

    new feature.

---
+++ 01vite-react/src/Rushi.jsx
@@ -0,0 +1,10 @@
+function Jamun() {
+    return (
+        <>
+            <h1>Rushi Jamun Tayar Ahe</h1>
+            <p>Lorem ipsum dolor sit amet.</p>
+        </>
+    );
+};
+
+export default Jamun;
