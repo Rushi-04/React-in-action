63c2260473584e031a72b2e114f03845a49ac9d5
Rushi-04 <borkarananta028@gmail.com>
2025-06-21 09:18:23 +0530

    Done with counter project.

--- 02Counter/src/App.jsx
+++ 02Counter/src/App.jsx
@@ -4,11 +4,37 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  
+  const [counter, setCounter] = useState(10);
+  // let counter = 5;
+  const AddValue = () => {
+    if (counter > 20) {
+      console.error("Counter exceeds 20 can't increase value")
+    }else{
+      setCounter(counter + 1);
+    }
+  }
+  
+  const SubValue = () => {
+    if (counter < 1) {
+      console.error("Counter reached 0 can't decrease value")
+    }
+    else {
+      setCounter(counter - 1);
+    }
+  }
   return (
     <>
-      <h1>Hello this is Rushi and Lets Start Our New React Project.</h1>
+      <h1>Welcome to counter</h1>
+      <h1>Counter is : {counter}</h1>
+      <div>
+        <button onClick={AddValue}>Add Value</button>
+        <button onClick={SubValue}>Sub Value</button>
+      </div>
+      <h1>Counter is : {counter}</h1>
+      <h1>Counter is : {counter}</h1>
+      <h1>Counter is : {counter}</h1>
+
     </>
   )
 }
