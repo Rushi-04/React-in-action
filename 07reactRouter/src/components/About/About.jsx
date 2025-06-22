37b4007c192fc56769c6d70d8af6867a5853df59
Rushi-04 <borkarananta028@gmail.com>
2025-06-22 22:43:25 +0530

    Done

--- 07reactRouter/src/components/About/About.jsx
+++ 07reactRouter/src/components/About/About.jsx
@@ -9,7 +9,7 @@ export default function About() {
                         <img
                             src="https://assets.designtemplate.io/Social-Media-Influencer-Girl-Vector-Illustration-700.webp"
                             alt="image"
-                        />
+                        /> 
                     </div>
                     <div className="md:7/12 lg:w-6/12">
                         <h2 className="text-2xl text-gray-900 font-bold md:text-4xl">
