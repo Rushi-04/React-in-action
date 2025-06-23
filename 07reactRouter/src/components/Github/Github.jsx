60ea396d7600a5c6c26581abea4187b3169b04d4
Rushi-04 <borkarananta028@gmail.com>
2025-06-23 15:35:06 +0530

    react router done

--- 07reactRouter/src/components/Github/Github.jsx
+++ 07reactRouter/src/components/Github/Github.jsx
@@ -1,24 +1,33 @@
 import React, { useEffect, useState } from 'react'
+import { useLoaderData } from 'react-router-dom'
+
 
 const Github = () => {
-
-    const [data, setData] = useState({});
-
+  
+  const [data, setData] = useState({});
+  
     useEffect(() => {
-      fetch('https://api.github.com/Rushi-04')
+      fetch('https://api.adviceslip.com/advice')
       .then(responce => responce.json())
-      .then(data => {
-        console.log(data)
-        setData(data)
+      .then(data1 => {
+        setData(data1.slip)
       })
     }, [])
+    // const data = useLoaderData();
+    // console.log(data)
 
   return (
-    <div className='text-center text-white bg-teal-500 rounded-2xl p-3 text-2xl font-medium m-2 '>
-        Github id: {data.id}
-        <img src={data.avatar_url} alt="" />
+    <div className='text-center text-white bg-gray-600 rounded-2xl p-3 text-2xl font-medium m-2 '>
+        {data ? `Life Advice: ${data.advice}` : 'Loading...'}
+        <img src={null} alt="" />
         </div>
   )
 }
 
 export default Github
+
+// export const getJoke = async() => {
+//   const responce = await fetch('https://api.adviceslip.com/advice')
+//   const data = responce.json()
+//   return data.slip
+// }
