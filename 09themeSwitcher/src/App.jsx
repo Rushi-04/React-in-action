1eb02fc2c8e2f3000ed5f26698c8044ff87db92d
Rushi-04 <borkarananta028@gmail.com>
2025-06-24 22:47:22 +0530

    new project done

--- 09themeSwitcher/src/App.jsx
+++ 09themeSwitcher/src/App.jsx
@@ -12,7 +12,7 @@ function App() {
       <div className="w-full">
         <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
           {/* themeBtn */}
-
+  
         </div>
 
         <div className="w-full max-w-sm mx-auto">
