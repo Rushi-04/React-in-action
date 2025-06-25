16a890ed71ee19328103e77b6a52240b614214b0
Rushi-04 <borkarananta028@gmail.com>
2025-06-25 20:31:15 +0530

    implemented dark mode

--- 09themeSwitcher/src/App.jsx
+++ 09themeSwitcher/src/App.jsx
@@ -1,24 +1,42 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { ThemeProvider } from './contexts/theme'
+import ThemeBtn from './components/ThemeButton'
+import Card from './components/Card'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [themeMode, setThemeMode] = useState("light")
+
+  const darkTheme = () => {
+    setThemeMode("dark")
+  }
+  
+  const lightTheme = () => {
+    setThemeMode("light")
+  }
+
+  useEffect(() => {
+    document.querySelector('html').classList.remove("light", "dark")
+    document.querySelector('html').classList.add(themeMode)
+  }, [themeMode])
+  
 
   return (
-
-    <div className="flex flex-wrap min-h-screen items-center">
-      <div className="w-full">
+    <ThemeProvider value={{themeMode, darkTheme, lightTheme}}>
+      <div className="flex flex-wrap min-h-screen items-center">
+      <div className="w-full">  
         <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
-          {/* themeBtn */}
+          <ThemeBtn/>
         </div>
 
         <div className="w-full max-w-sm mx-auto">
-          {/* Card */}
+          <Card/>
         </div>
       </div>
     </div>
+    </ThemeProvider>
 
 
 
